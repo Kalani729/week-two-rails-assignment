2a8870562d722bc01c86075ce040fb12dac08f02
Mark Albright <kalani729@gmail.com>
2017-08-14 17:42:59 -0600

    Finished rspec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_helper
+    if admin_signed_in?
+    (link_to "Logout", destroy_admin_session_path, method: :delete) + " ".html_safe +
+    (link_to current_admin.email, edit_admin_registration_path)
+    else
+    (link_to "Sign In", new_admin_session_path) + " ".html_safe +
+    (link_to "Register", new_admin_registration_path)
+    end
+  end     
 end
